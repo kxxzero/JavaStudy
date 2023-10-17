79671b36c4e5ea11f9c62ba98578c83396c04b41
SIST <SIST@DESKTOP-QCQ9VG5>
2023-10-17 18:13:50 +0900

    11111

---
+++ Chapter06_자바상속_5/src/com/sist/main2/MainClass4.java
@@ -0,0 +1,29 @@
+package com.sist.main2;
+
+class Super
+{
+	
+}
+
+class Sub extends Super
+{
+	
+}
+
+// NullPointerException, ClassCastException
+public class MainClass4 {
+	static String str; // null
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Super ssss;
+		Super s=new Sub();
+//		Sub ss=(Sub)new Super(); // ClassCastException
+//		Sub ss=new Super();
+//		Sub ss=(Sub)s;
+		char c=str.charAt(0);
+//		Super sss=new Super();
+//		Sub ssss=(Sub)ssss;
+	}
+
+}
