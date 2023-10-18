d0998260d195456886fa15953ab65ca05fe247c8
SIST <SIST@DESKTOP-QCQ9VG5>
2023-10-18 16:54:27 +0900

    0

---
+++ 자바 3주차 정리/src/com/sist/main/MainClass5.java
@@ -0,0 +1,40 @@
+package com.sist.main;
+
+// 캡슐화
+class Member
+{
+	
+	// 변수 은닉화
+	private int age; // 0
+	private String name; // null => 읽기/쓰기
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
+
+public class MainClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Member m=new Member(); // m(age, name)
+		m.setAge(20); // m.age
+		m.setName(null); // m.name
+		
+		System.out.println(m.getAge());
+		System.out.println(m.getName());
+		
+	}
+
+}
