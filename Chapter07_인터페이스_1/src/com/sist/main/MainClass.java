fc3c4f5b528d0c30684c01be6b613f55013e9765
SIST <SIST@DESKTOP-QCQ9VG5>
2023-10-18 17:04:34 +0900

    0

---
+++ Chapter07_인터페이스_1/src/com/sist/main/MainClass.java
@@ -0,0 +1,52 @@
+package com.sist.main;
+
+/*
+ * 	interface
+ * 		- 서로 다른 클래스를 연결해서 사용
+ * 		- 결합성이 낮은 프로그램
+ * 		- 개발자가 같은 메소드를 사용 => 표준화
+ * 		- 관련된 모든 클래스를 묶어서 1개의 이름으로 관리
+ * 			=> Model => 형식 => interface
+ * 			=> class A
+ * 			   class B extends A
+ * 
+ * 				A a=new A() // a에 있는 변수, 메소드만 호출
+ * 				B b=new B() // b에 있는 변수, 메소드만 호출
+ * 				A c=new B()	// a에 있는 변수, 메소드는 B에 있는 것을 호출 => 오버라이드된 메소드를 사용
+ * 				
+ * 				interface I => aaa() => 클래스마다 공통으로 적용되는 메소드 선언
+ * 				class A implements I
+ * 				{
+ * 					aaa() bbb()
+ * 				}
+ * 					I i=new A() (X) // A a=new A();
+ * 				class B implements I
+ * 				class C implements I
+ * 
+ * 				I i=new A(); // i.aaa() => i에 존재하는 메소드 중에 A에서 오버라이딩한 메소드 호출
+ * 				I i=new B(); // 
+ * 				I i=new C();
+ * 
+ * 			class A
+ * 			{
+ * 				int a;
+ * 				void aaa(){}
+ * 			}
+ * 			Object obj=new A();
+ * 			
+ * 			class B
+ * 			class C
+ * 			Object[] obj={new A(), new B(), new C()}
+ * 
+ * 			A a=(A)obj[0];
+ * 			
+ */
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
